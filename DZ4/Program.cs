8d4ab1b46411ba6174188961cb2e2ba3dcafa895
Admin <ge1983@yandex.ru>
2023-02-27 17:22:59 +1000

    Добавлено задание 4

---
+++ DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Ввелите число N: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+int index=2;
+
+while (index <= N)
+{
+    Console.Write($"{index} ");
+    index = index + 2;
+}
