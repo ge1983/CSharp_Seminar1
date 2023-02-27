0441fe72dbddc098efef0610468c9eb6149e6c91
Admin <ge1983@yandex.ru>
2023-02-27 17:04:34 +1000

    Добавлено задание 1

---
+++ DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Ввелите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Ввелите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine()); 
+
+if (Number1 < Number2)
+{
+    Console.WriteLine($"{Number1} - минимальное");
+    Console.WriteLine($"{Number2} - максимальное");
+}
+else
+{
+    Console.WriteLine($"{Number1} - максимальное");
+    Console.WriteLine($"{Number2} - минимальное");
+}
